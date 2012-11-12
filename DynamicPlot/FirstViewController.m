d6c159e65a98702a1a5773e70b0fa8224519e85e
Elenka <alekseenko.lena@gmail.com>
2012-11-12 19:56:54 +0400

    axes

--- DynamicPlot/FirstViewController.m
+++ DynamicPlot/FirstViewController.m
@@ -236,21 +236,97 @@
 	return NO;
 }
 
-/*
 
--(void) accelerometerReciver:(NSNotification*)theNotice{
-    [self graphDrow:( (CMAccelerometerData*)[theNotice.userInfo objectForKey:@"accel"] ).acceleration];
+
+
+-(void)configureAxes {
+	// 1 - Create styles
+	CPTMutableTextStyle *axisTitleStyle = [CPTMutableTextStyle textStyle];
+	axisTitleStyle.color = [CPTColor whiteColor];
+	axisTitleStyle.fontName = @"Helvetica-Bold";
+	axisTitleStyle.fontSize = 12.0f;
+	CPTMutableLineStyle *axisLineStyle = [CPTMutableLineStyle lineStyle];
+	axisLineStyle.lineWidth = 2.0f;
+	axisLineStyle.lineColor = [CPTColor whiteColor];
+	CPTMutableTextStyle *axisTextStyle = [[CPTMutableTextStyle alloc] init];
+	axisTextStyle.color = [CPTColor whiteColor];
+	axisTextStyle.fontName = @"Helvetica-Bold";
+	axisTextStyle.fontSize = 11.0f;
+	CPTMutableLineStyle *tickLineStyle = [CPTMutableLineStyle lineStyle];
+	tickLineStyle.lineColor = [CPTColor whiteColor];
+	tickLineStyle.lineWidth = 2.0f;
+	CPTMutableLineStyle *gridLineStyle = [CPTMutableLineStyle lineStyle];
+	tickLineStyle.lineColor = [CPTColor blackColor];
+	tickLineStyle.lineWidth = 1.0f;
+	// 2 - Get axis set
+	CPTXYAxisSet *axisSet = (CPTXYAxisSet *) self.hostView.hostedGraph.axisSet;
+	// 3 - Configure x-axis
+	CPTAxis *x = axisSet.xAxis;
+	x.title = @"Day of Month";
+	x.titleTextStyle = axisTitleStyle;
+	x.titleOffset = 15.0f;
+	x.axisLineStyle = axisLineStyle;
+	x.labelingPolicy = CPTAxisLabelingPolicyNone;
+	x.labelTextStyle = axisTextStyle;
+	x.majorTickLineStyle = axisLineStyle;
+	x.majorTickLength = 4.0f;
+	x.tickDirection = CPTSignNegative;
+	CGFloat dateCount = [[[CPDStockPriceStore sharedInstance] datesInMonth] count];
+	NSMutableSet *xLabels = [NSMutableSet setWithCapacity:dateCount];
+	NSMutableSet *xLocations = [NSMutableSet setWithCapacity:dateCount];
+	NSInteger i = 0;
+	for (NSString *date in [[CPDStockPriceStore sharedInstance] datesInMonth]) {
+		CPTAxisLabel *label = [[CPTAxisLabel alloc] initWithText:date  textStyle:x.labelTextStyle];
+		CGFloat location = i++;
+		label.tickLocation = CPTDecimalFromCGFloat(location);
+		label.offset = x.majorTickLength;
+		if (label) {
+			[xLabels addObject:label];
+			[xLocations addObject:[NSNumber numberWithFloat:location]];
+		}
+	}
+	x.axisLabels = xLabels;
+	x.majorTickLocations = xLocations;
+	// 4 - Configure y-axis
+	CPTAxis *y = axisSet.yAxis;
+	y.title = @"Price";
+	y.titleTextStyle = axisTitleStyle;
+	y.titleOffset = -40.0f;
+	y.axisLineStyle = axisLineStyle;
+	y.majorGridLineStyle = gridLineStyle;
+	y.labelingPolicy = CPTAxisLabelingPolicyNone;
+	y.labelTextStyle = axisTextStyle;
+	y.labelOffset = 16.0f;
+	y.majorTickLineStyle = axisLineStyle;
+	y.majorTickLength = 4.0f;
+	y.minorTickLength = 2.0f;
+	y.tickDirection = CPTSignPositive;
+	NSInteger majorIncrement = 100;
+	NSInteger minorIncrement = 50;
+	CGFloat yMax = 700.0f;  // should determine dynamically based on max price
+	NSMutableSet *yLabels = [NSMutableSet set];
+	NSMutableSet *yMajorLocations = [NSMutableSet set];
+	NSMutableSet *yMinorLocations = [NSMutableSet set];
+	for (NSInteger j = minorIncrement; j <= yMax; j += minorIncrement) {
+		NSUInteger mod = j % majorIncrement;
+		if (mod == 0) {
+			CPTAxisLabel *label = [[CPTAxisLabel alloc] initWithText:[NSString stringWithFormat:@"%i", j] textStyle:y.labelTextStyle];
+			NSDecimal location = CPTDecimalFromInteger(j);
+			label.tickLocation = location;
+			label.offset = -y.majorTickLength - y.labelOffset;
+			if (label) {
+				[yLabels addObject:label];
+			}
+			[yMajorLocations addObject:[NSDecimalNumber decimalNumberWithDecimal:location]];
+		} else {
+			[yMinorLocations addObject:[NSDecimalNumber decimalNumberWithDecimal:CPTDecimalFromInteger(j)]];
+		}
+	}
+	y.axisLabels = yLabels;
+	y.majorTickLocations = yMajorLocations;
+	y.minorTickLocations = yMinorLocations;
 }
 
--(void) motionReciver:(NSNotification*)theNotice{
-    CMAcceleration acc;
-    acc.x=( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).gravity.x+( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).userAcceleration.x;
-    acc.y=( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).gravity.y+( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).userAcceleration.y;
-    acc.z=( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).gravity.z+( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).userAcceleration.z;
-    [self graphDrow:acc];
-}
-
-*/
 -(void)viewDidUnload {
 	[super viewDidUnload];
 }
@@ -279,7 +355,21 @@
 
 
 
-
+/*
+ 
+ -(void) accelerometerReciver:(NSNotification*)theNotice{
+ [self graphDrow:( (CMAccelerometerData*)[theNotice.userInfo objectForKey:@"accel"] ).acceleration];
+ }
+ 
+ -(void) motionReciver:(NSNotification*)theNotice{
+ CMAcceleration acc;
+ acc.x=( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).gravity.x+( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).userAcceleration.x;
+ acc.y=( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).gravity.y+( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).userAcceleration.y;
+ acc.z=( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).gravity.z+( (CMDeviceMotion*)[theNotice.userInfo objectForKey:@"motion"]).userAcceleration.z;
+ [self graphDrow:acc];
+ }
+ 
+ */
 
 
 
